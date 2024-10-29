b021581a1ef6a85456f55d347644e5fff9d0e2e0
Squair <squairyt@gmail.com>
2024-10-29 18:24:32 -0400

    modified:   main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import * as THREE from './three.module.js'
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -40,7 +39,6 @@ scene.add(ambientLight, pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderrer.domElement);
 
 function stars(){
  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
